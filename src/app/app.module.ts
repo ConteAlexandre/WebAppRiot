fdf2a560d00543b27673875ac7fae5dddd0f490b
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-10-08 16:22:02 +0200

    Add the component connection and merge develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {ConnectionComponent} from './connection/connection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ConnectionComponent
   ],
   imports: [
     BrowserModule,
